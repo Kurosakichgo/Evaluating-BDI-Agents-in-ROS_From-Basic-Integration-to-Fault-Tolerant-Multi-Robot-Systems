3db285982f3b80043717e56af637a34ad32e9cda
Qinhan Du <74041193+Kurosakichgo@users.noreply.github.com>
2025-06-23 00:51:07 -0700

    Env.java

--- Faulty_Tolernce1/src/java/Env.java
+++ Faulty_Tolernce1/src/java/Env.java
@@ -72,12 +72,12 @@ public class Env extends Environment {
         switch (action.getFunctor()) {
             case "select_best_bid":
                 return select_best_bid(action);
+            case "select_best_bid_f":
+                return select_best_bid(action);
             case "calculate_distance_for_bid":
                 return calculate_distance_for_bid(agName, action);
-            case "append":
-                return append(agName, action);
-            case "add_bid":
-                return add_bid(action);
+            case"calculate_distance_for_bid_f":
+                return calculate_distance_for_bid_f(agName, action);
             case "send_goal":
                 return send_goal(action);
             default:
@@ -101,13 +101,16 @@ public class Env extends Environment {
             // 遍历所有 bids
             for (Term bidTerm : bidsList) {
                 Structure bid = (Structure) bidTerm;
-                Term goalId = bid.getTerm(1);
-                double bidValue = ((NumberTerm) bid.getTerm(4)).solve();
+                 Term  gx      = bid.getTerm(1);              // 故障点 X
+                 Term  gy      = bid.getTerm(2); 
+                double bidValue = ((NumberTerm) bid.getTerm(3)).solve();
+                ListTerm coordKey = ASSyntax.createList(gx, gy);
+
     
                 // 如果该 GoalId 没有记录，或者当前 bid 更优，则更新
-                if (!bestBids.containsKey(goalId) || bidValue < bestValues.get(goalId)) {
-                    bestBids.put(goalId, bid);
-                    bestValues.put(goalId, bidValue);
+                if (!bestBids.containsKey(coordKey) || bidValue < bestValues.get(coordKey)) {
+                    bestBids.put(coordKey, bid);
+                    bestValues.put(coordKey, bidValue);
                 }
             }
     
@@ -116,11 +119,10 @@ public class Env extends Environment {
             for (Map.Entry<Term, Structure> entry : bestBids.entrySet()) {
                 Structure bestBid = entry.getValue();
                 Term bestAgent = bestBid.getTerm(0);
-                Term goalId = bestBid.getTerm(1);
-                Term gx = bestBid.getTerm(2);
-                Term gy = bestBid.getTerm(3);
-                Term bestDistance = bestBid.getTerm(4);
-                Literal bestBidLiteral = ASSyntax.createLiteral("best_bid", bestAgent, goalId, gx, gy, bestDistance);
+                Term gx = bestBid.getTerm(1);
+                Term gy = bestBid.getTerm(2);
+                Term bestDistance = bestBid.getTerm(3);
+                Literal bestBidLiteral = ASSyntax.createLiteral("best_bid", bestAgent, gx, gy, bestDistance);
                 addPercept(bestBidLiteral);
             }
     
@@ -133,7 +135,53 @@ public class Env extends Environment {
     }
     
     
-   
+   public boolean select_best_bid_f(Structure action) {
+        try {
+            ListTerm bidsList = (ListTerm) action.getTerm(0);
+            if (bidsList.size() == 0) {
+                logger.warning("No bids received.");
+                return false;
+            }
+    
+            // 存储每个 GoalId 最佳的 bid
+            Map<Term, Structure> bestBids = new HashMap<>();
+            Map<Term, Double> bestValues = new HashMap<>();
+    
+            // 遍历所有 bids
+            for (Term bidTerm : bidsList) {
+                Structure bid = (Structure) bidTerm;
+                 Term  gx      = bid.getTerm(1);              // 故障点 X
+                 Term  gy      = bid.getTerm(2); 
+                double bidValue = ((NumberTerm) bid.getTerm(3)).solve();
+                ListTerm coordKey = ASSyntax.createList(gx, gy);
+
+    
+                // 如果该 GoalId 没有记录，或者当前 bid 更优，则更新
+                if (!bestBids.containsKey(coordKey) || bidValue < bestValues.get(coordKey)) {
+                    bestBids.put(coordKey, bid);
+                    bestValues.put(coordKey, bidValue);
+                }
+            }
+    
+            // 将最佳的 bid 作为感知加入
+            clearPercepts();
+            for (Map.Entry<Term, Structure> entry : bestBids.entrySet()) {
+                Structure bestBid = entry.getValue();
+                Term bestAgent = bestBid.getTerm(0);
+                Term gx = bestBid.getTerm(1);
+                Term gy = bestBid.getTerm(2);
+                Term bestDistance = bestBid.getTerm(3);
+                Literal bestBidLiteral = ASSyntax.createLiteral("best_bid_f", bestAgent, gx, gy, bestDistance);
+                addPercept(bestBidLiteral);
+            }
+    
+            bids.clear(); // 清空 bids 列表
+            return true;
+        } catch (Exception e) {
+            logger.warning("Error selecting best bid: " + e.getMessage());
+            return false;
+        }
+    }
 
     public boolean calculate_distance_for_bid(String agName, Structure action) {
         try {
@@ -142,12 +190,11 @@ public class Env extends Environment {
             double y = ((NumberTerm) action.getTerm(1)).solve();
             double gx = ((NumberTerm) action.getTerm(2)).solve();
             double gy = ((NumberTerm) action.getTerm(3)).solve();
-            String goalId = ((Atom) action.getTerm(5)).toString();
 
             double distance = calculateManhattanDistance(x, y, gx, gy);
             String agent = ((Atom) action.getTerm(4)).toString();
             logger.info("Calculated distance: " + distance + " for goal (" + gx + ", " + gy + ")");
-            addPercept(agName, Literal.parseLiteral("distance(" + goalId + "," + agent + ", " + gx + ", " + gy + ", " + distance + ")"));
+            addPercept(agName, Literal.parseLiteral("distance(" + agent + ", " + gx + ", " + gy + ", " + distance + ")"));
             return true;
         } catch (Exception e) {
             logger.warning("Error calculating distance: " + e.getMessage());
@@ -159,29 +206,36 @@ public class Env extends Environment {
         return Math.abs(gx - x) + Math.abs(gy - y);
     }
     
-    public boolean append(String agName, Structure action) {
-        try {
-            Literal bid = (Literal) action.getTerm(0);
-            bids.add(bid);
-            return true;
-        } catch (Exception e) {
-            logger.warning("Error appending bid: " + e.getMessage());
-            return false;
-        }
-    }
-    
+   
 
-    public boolean add_bid(Structure action) {
+
+    
+    
+public boolean calculate_distance_for_bid_f(String agName, Structure action) {
         try {
-            Literal bid = ASSyntax.createLiteral("bid", action.getTermsArray());
-            bids.add(bid);
-            logger.info("Stored bid: " + bid);
+           
+            double x = ((NumberTerm) action.getTerm(0)).solve();
+            double y = ((NumberTerm) action.getTerm(1)).solve();
+            double gx = ((NumberTerm) action.getTerm(2)).solve();
+            double gy = ((NumberTerm) action.getTerm(3)).solve();
+
+            double distance = calculateManhattanDistance_f(x, y, gx, gy);
+            String agent = ((Atom) action.getTerm(4)).toString();
+            logger.info("Calculated distance: " + distance + " for goal (" + gx + ", " + gy + ")");
+            addPercept(agName, Literal.parseLiteral("distance_f(" + agent + ", " + gx + ", " + gy + ", " + distance + ")"));
             return true;
         } catch (Exception e) {
-            logger.warning("Error storing bid: " + e.getMessage());
+            logger.warning("Error calculating distance: " + e.getMessage());
             return false;
         }
     }
+
+    private double calculateManhattanDistance_f(double x, double y, double gx, double gy) {
+        return Math.abs(gx - x) + Math.abs(gy - y);
+    }
+
+
+   
 
    public boolean send_goal(Structure action) {
     try {
